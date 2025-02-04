98e690c1a776a9f094de2afd1048e82e93315bcf
iamzlatika <mazurovazv@gmail.com>
2025-02-04 16:41:40 +0200

    Implement input search

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,6 +7,21 @@ export type PostWithDetails = Post & {
   _count: { comments: number };
 };
 
+export function fetchPostsBySearchTerm(
+  term: string
+): Promise<PostWithDetails[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [{ title: { contains: term } }, { content: { contains: term } }],
+    },
+  });
+}
+
 export function fetchPostByTopicSlug(slug: string): Promise<PostWithDetails[]> {
   return db.post.findMany({
     where: { topic: { slug } },
